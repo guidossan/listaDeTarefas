82492153b365d3949df27a1b00eca2605f73d346
Guilherme Martins <guimsl2004@gmail.com>
2025-01-31 11:05:07 -0300

    intem component

--- listaTarefas/src/app/components/list-item-component/list-item-component.component.ts
+++ listaTarefas/src/app/components/list-item-component/list-item-component.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IListItems } from '../../interface/IListItems.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-list-item-component',
-  imports: [],
+  standalone:true,
+  imports: [CommonModule],
   templateUrl: './list-item-component.component.html',
   styleUrl: './list-item-component.component.scss'
 })
 export class ListItemComponentComponent {
+
+  @Input({ required: true }) public inputListItems: IListItems[] = []
 
 }
